8aee13ff9c2969cc2eadf23c5709ad54ed05f100
tituslesley89 <tituslesley89@gmail.com>
2022-04-27 21:19:37 -0700

    Initial commit

---
+++ test/quiddler-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as QuiddlerBackend from '../lib/quiddler-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/quiddler-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new QuiddlerBackend.QuiddlerBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
